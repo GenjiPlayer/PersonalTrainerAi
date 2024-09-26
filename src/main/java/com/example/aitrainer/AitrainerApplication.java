a44bda9e312fa56cad290ac36d65061c37ecd263
yps1lon <benjamin.mahic1@gmail.com>
2024-09-26 12:20:49 +0200

    inshallah

---
+++ src/main/java/com/example/aitrainer/AitrainerApplication.java
@@ -0,0 +1,13 @@
+package com.example.aitrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitrainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AitrainerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aitrainer/AitrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aitrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitrainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
