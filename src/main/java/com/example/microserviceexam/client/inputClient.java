c779cf9f22d7bcfa1724ac20f8661669087863ac
GenjiPlayer <147660352+GenjiPlayer@users.noreply.github.com>
2024-10-02 15:38:55 +0200

    Slightly Red Application

---
+++ src/main/java/com/example/microserviceexam/client/inputClient.java
@@ -0,0 +1,8 @@
+package com.example.microserviceexam.client;
+
+import com.example.microserviceexam.dto.userDTO;
+import org.springframework.http.ResponseEntity;
+
+public interface inputClient {
+   ResponseEntity<String> input(userDTO userDTO);
+}
