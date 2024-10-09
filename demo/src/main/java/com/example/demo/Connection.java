3162e14fe1ecd736c47bc74312a4ff27c297939e
Sveinung <sveinung.ulstein@hotmail.com>
2024-10-09 14:34:31 +0200

    hente øvelser funker, må bare lage inputs for å kunne spesifisere øvelse

--- demo/src/main/java/com/example/demo/Connection.java
+++ demo/src/main/java/com/example/demo/Connection.java
@@ -1,19 +1,38 @@
 package com.example.demo;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 public class Connection {
-    URL url;
-    {
-        try {
-            url = new URL("https://api.api-ninjas.com/v1/exercises");
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
+    public String muscle = "biceps";
+
+    public void getTestResponse(){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "https://api.api-ninjas.com/v1/exercises?" + "muscle=" + muscle;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("X-Api-Key", "sWmCsF8THa9NbTHeceZSIw==1iZ4GiP2bL4gDJfp");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                url, HttpMethod.GET, entity, String.class);
+
+
+
+        System.out.println(response.getBody());
+
+    }
 }
+
