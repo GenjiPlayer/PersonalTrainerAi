47753dd847d32b396471943e7a5367465c4fc875
yps1lon <benjamin.mahic1@gmail.com>
2024-11-08 15:51:33 +0100

    lolololol fixed working 100%

--- src/main/java/com/example/microserviceexam/servletCheck/webConfig.java
+++ src/main/java/com/example/microserviceexam/servletCheck/webConfig.java
@@ -16,7 +16,7 @@ public class webConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "http://localhost8081")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
