71d8f3e776a52d94520df53c925176f322281ee8
yps1lon <benjamin.mahic1@gmail.com>
2024-12-16 10:29:31 +0100

    more microservice theory

--- src/main/java/com/example/microserviceexam/client/inputClient.java
+++ src/main/java/com/example/microserviceexam/client/inputClient.java
@@ -1,8 +1,23 @@
 package com.example.microserviceexam.client;
-
 import com.example.microserviceexam.dto.userDTO;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+@Component
+public class inputClient implements inputClientImp {
+    @Autowired
+    private RestTemplate restTemplate;
 
-public interface inputClient {
-   ResponseEntity<String> input(userDTO userDTO);
+    private final String refactorService = "http://localhost:8090/refactor/enriched";
+
+    @Override
+    public void refactor(userDTO userdto) {
+        try{
+            Object res = restTemplate.postForObject(refactorService, userdto, Object.class);
+            System.out.println("YIPPIE" + res);
+        } catch (Exception e) {
+            System.out.println("womp womp" + e.getMessage());
+        }
+    }
+
 }
--- src/main/java/com/example/microserviceexam/client/inputClientImp.java
+++ src/main/java/com/example/microserviceexam/client/inputClientImp.java
@@ -1,30 +1,12 @@
-/*package com.example.microserviceexam.client;
+package com.example.microserviceexam.client;
 
 import com.example.microserviceexam.dto.userDTO;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
+import com.example.microserviceexam.model.userInput;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
-import javax.transaction.Transactional;
+import java.util.List;
 
+public interface inputClientImp {
 
-@Component
-public class inputClientImp implements inputClient {
-
-    private final RestTemplate restTemplate = new RestTemplate();
-    private final String serviceURL = "http://localhost:8080/input/test";
-
-    @Override
-    public ResponseEntity<String> input(userDTO userDTO) {
-        HttpEntity<userDTO> request = new HttpEntity<>(userDTO);
-
-        return restTemplate.exchange(
-                serviceURL,
-                HttpMethod.POST,
-                request,
-                String.class
-        );
-    }
-}*/
+  void refactor(userDTO userDTO);
+}
