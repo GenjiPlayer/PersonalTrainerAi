6f2d21b40eb820aa449bcf753fc1c713cd686403
Ole <ole.magnus.letrud@gmail.com>
2024-12-17 20:50:39 +0100

    ting i service og clien ble ikke adda???? her er d. surley bruker vi noe jeg lager? :|

--- foodGenerator/src/main/java/com/example/foodProgram/client/ExternalAPIClient.java
+++ foodGenerator/src/main/java/com/example/foodProgram/client/ExternalAPIClient.java
@@ -1,4 +1,55 @@
 package com.example.foodProgram.client;
 
-public class ExternalAPIClient {
+import com.example.foodProgram.model.foodModel;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+    @Component
+    public class ExternalAPIClient {
+//adder api ting her::::
+        public List<foodModel> fetchGainWeightMeals() {
+            return Arrays.asList(
+                    new foodModel(
+                            "Stracciatella (Italian Wedding Soup)",
+                            "3 1/2 c Chicken broth; homemade|1 lb Fresh spinach; wash/trim/chop|1 Egg|1 c Grated parmesan cheese|Salt and pepper; to taste",
+                            "Bring 1 cup of the broth to a boil. Add spinach and cook until softened but still bright green. Remove spinach with a slotted spoon and set aside. " +
+                                    "Add remaining broth to pot. Bring to a boil. Meanwhile, beat egg lightly with a fork. Beat in 1/4 cup of cheese. When broth boils pour in egg mixture, stirring constantly for a few seconds until it cooks into 'rags'. " +
+                                    "Add reserved spinach, salt and pepper. Serve immediately, passing remaining cheese.",
+                            "4 servings"
+                    ),
+                    new foodModel(
+                            "Stracciatella (Italian Wedding Soup)",
+                            "3 1/2 c Chicken broth; homemade|1 lb Fresh spinach; wash/trim/chop|1 Egg|1 c Grated parmesan cheese|Salt and pepper; to taste",
+                            "Bring 1 cup of the broth to a boil. Add spinach and cook until softened but still bright green. Remove spinach with a slotted spoon and set aside. " +
+                                    "Add remaining broth to pot. Bring to a boil. Meanwhile, beat egg lightly with a fork. Beat in 1/4 cup of cheese. When broth boils pour in egg mixture, stirring constantly for a few seconds until it cooks into 'rags'. " +
+                                    "Add reserved spinach, salt and pepper. Serve immediately, passing remaining cheese.",
+                            "4 servings"
+                    )
+            );
+        }
+
+        public List<foodModel> fetchLoseWeightMeals() {
+            return Arrays.asList(
+                    new foodModel(
+                            "Stracciatella (Italian Wedding Soup)",
+                            "3 1/2 c Chicken broth; homemade|1 lb Fresh spinach; wash/trim/chop|1 Egg|1 c Grated parmesan cheese|Salt and pepper; to taste",
+                            "Bring 1 cup of the broth to a boil. Add spinach and cook until softened but still bright green. Remove spinach with a slotted spoon and set aside. " +
+                                    "Add remaining broth to pot. Bring to a boil. Meanwhile, beat egg lightly with a fork. Beat in 1/4 cup of cheese. When broth boils pour in egg mixture, stirring constantly for a few seconds until it cooks into 'rags'. " +
+                                    "Add reserved spinach, salt and pepper. Serve immediately, passing remaining cheese.",
+                            "4 servings"
+                    ),
+                    new foodModel(
+                            "Stracciatella (Italian Wedding Soup)",
+                            "3 1/2 c Chicken broth; homemade|1 lb Fresh spinach; wash/trim/chop|1 Egg|1 c Grated parmesan cheese|Salt and pepper; to taste",
+                            "Bring 1 cup of the broth to a boil. Add spinach and cook until softened but still bright green. Remove spinach with a slotted spoon and set aside. " +
+                                    "Add remaining broth to pot. Bring to a boil. Meanwhile, beat egg lightly with a fork. Beat in 1/4 cup of cheese. When broth boils pour in egg mixture, stirring constantly for a few seconds until it cooks into 'rags'. " +
+                                    "Add reserved spinach, salt and pepper. Serve immediately, passing remaining cheese.",
+                            "4 servings"
+                    )
+            );
+        }
+    }
+
 }
--- foodGenerator/src/main/java/com/example/foodProgram/foodService/foodService.java
+++ foodGenerator/src/main/java/com/example/foodProgram/foodService/foodService.java
@@ -1,4 +1,26 @@
 package com.example.foodProgram.foodService;
 
+import com.example.foodProgram.client.ExternalAPIClient;
+import com.example.foodProgram.model.foodModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class foodService {
+    private final ExternalAPIClient externalAPIClient;
+
+    @Autowired
+    public foodService(ExternalAPIClient externalAPIClient){
+        this.externalAPIClient = externalAPIClient;
+    }
+    public List<foodModel> generateEatingPlan(String goal) {
+        if (/*something from benjamin sin ting*/) {
+            return externalAPIClient.fetchGainWeightMeals();
+        } else if (/*something from benjamin sin ting*/) {
+            return externalAPIClient.fetchLoseWeightMeals();
+        }
+        throw new IllegalArgumentException("Invalid goal.");
+    }
 }
