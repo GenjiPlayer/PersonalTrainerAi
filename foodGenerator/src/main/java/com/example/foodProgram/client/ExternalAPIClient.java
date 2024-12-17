a2c5a4c88969ea75e419ce17f03b2012ee53c8ed
Ole <ole.magnus.letrud@gmail.com>
2024-12-17 21:17:05 +0100

    Made shit runnable

--- foodGenerator/src/main/java/com/example/foodProgram/client/ExternalAPIClient.java
+++ foodGenerator/src/main/java/com/example/foodProgram/client/ExternalAPIClient.java
@@ -51,5 +51,3 @@ import java.util.List;
             );
         }
     }
-
-}
--- foodGenerator/src/main/java/com/example/foodProgram/foodProgramApplication.java
+++ foodGenerator/src/main/java/com/example/foodProgram/foodProgramApplication.java
@@ -1,8 +1,10 @@
 package com.example.foodProgram;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+
 public class foodProgramApplication {
-
     public static void main(String[] args) {
-
-    }
+        SpringApplication.run(foodProgramApplication.class, args);    }
 }
